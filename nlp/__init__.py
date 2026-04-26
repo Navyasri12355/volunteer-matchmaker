bcb710e862eef204ff1bbbc93a4e3faabf58139b
Navyasri Mahitha Pulipati <142398240+Navyasri12355@users.noreply.github.com>
2026-04-26 08:21:23 +0530

    feat(nlp): expose unified api for nlp package

---
+++ nlp/__init__.py
@@ -0,0 +1,48 @@
+"""
+nlp
+---
+Public API for the NLP package.
+
+All external callers (API routers, event_service, etc.) should import
+from here.  Internal modules may import from each other directly.
+
+    from nlp import SeverityEngine, EventInput, SeverityResult
+    from nlp import CategoryConfig, CATEGORIES
+    from nlp import EventNLPExtractor, ExtractedEntities
+    from nlp import TrustScorer, SkillVerifier
+"""
+
+from nlp.category_config import CategoryConfig, EventCategory, CATEGORIES
+from nlp.severity_engine import (
+    SeverityEngine,
+    EventInput,
+    SeverityResult,
+    SeverityBand,
+    build_map_marker,
+)
+from nlp.event_nlp_extractor import EventNLPExtractor, ExtractedEntities
+from nlp.trust_scorer import TrustScorer, NGOTrustScore, VolunteerPointsLedger
+from nlp.skill_verifier import SkillVerifier, CertificateVerificationResult
+
+__all__ = [
+    # config
+    "CategoryConfig",
+    "EventCategory",
+    "CATEGORIES",
+    # severity
+    "SeverityEngine",
+    "EventInput",
+    "SeverityResult",
+    "SeverityBand",
+    "build_map_marker",
+    # extractor
+    "EventNLPExtractor",
+    "ExtractedEntities",
+    # trust
+    "TrustScorer",
+    "NGOTrustScore",
+    "VolunteerPointsLedger",
+    # skills
+    "SkillVerifier",
+    "CertificateVerificationResult",
+]
